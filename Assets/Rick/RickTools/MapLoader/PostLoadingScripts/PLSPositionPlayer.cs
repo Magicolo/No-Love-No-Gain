8b953bbb1b02f25c6f647e46fc57122444509f26
Magicolo <straty91@hotmail.com>
2015-10-22 12:01:37 -0400

    -Réperer les line endings...

--- Assets/Rick/RickTools/MapLoader/PostLoadingScripts/PLSPositionPlayer.cs
+++ Assets/Rick/RickTools/MapLoader/PostLoadingScripts/PLSPositionPlayer.cs
@@ -8,26 +8,26 @@ using Magicolo;
 public class PLSPositionPlayer : PostLoadingScript
 {
 
-    MapData mapData;
-    PlayerPositionData playerPositionData;
+	MapData mapData;
+	PlayerPositionData playerPositionData;
 
-    public override void init(MapData mapData)
-    {
-        playerPositionData = mapData.gameObject.AddComponent<PlayerPositionData>();
-    }
+	public override void init(MapData mapData)
+	{
+		playerPositionData = mapData.gameObject.AddComponent<PlayerPositionData>();
+	}
 
-    public override void addObject(string objectGroupName, int id, string objName, float x, float y, Dictionary<string, string> properties)
-    {
-        playerPositionData.positions[objName] = new Vector2(x, y);
-    }
+	public override void addObject(string objectGroupName, int id, string objName, float x, float y, Dictionary<string, string> properties)
+	{
+		playerPositionData.positions[objName] = new Vector2(x, y);
+	}
 
-    public override void addObjectGroups(string objectGroupName, Color color, Dictionary<string, string> properties)
-    {
-        if (objectGroupName == "Positions")
-        {
-            playerPositionData.drawColor = color;
-        }
-    }
+	public override void addObjectGroups(string objectGroupName, Color color, Dictionary<string, string> properties)
+	{
+		if (objectGroupName == "Positions")
+		{
+			playerPositionData.drawColor = color;
+		}
+	}
 
-    
+
 }
