52a41101ce7a21ee1ee72357a817f3b27cefebcb
Richard <Rail.rich@gmail.com>
2015-10-24 13:52:47 -0400

    OH YEA

--- Assets/Scripts/Characters/Civiles/Civile.cs
+++ Assets/Scripts/Characters/Civiles/Civile.cs
@@ -11,17 +11,16 @@ public class Civile : DamageableBase, IPoolable, ICopyable<Civile>
 	public float MovementSpeedMin;
 	public float MovementSpeedMax;
 
-	public Vector2 RaycastDirection;
-	public float RaycastDistance;
-	public LayerMask Mask;
-	[Disable]
-	public GameObject GameObjectOnNextFoot;
-
 	[DoNotCopy]
 	private Rigidbody2D body;
 
 	[DoNotCopy]
 	public bool LeftEntrance;
+
+	public LayerMask FlipWhenSeeing;
+	public SingleRayCast GroundRayCast;
+	[Disable, DoNotCopy]
+	public GameObject ForwardGroundGameObject;
 
 	void Start()
 	{
@@ -35,10 +34,6 @@ public class Civile : DamageableBase, IPoolable, ICopyable<Civile>
 		MovementSpeed = reference.MovementSpeed;
 		MovementSpeedMin = reference.MovementSpeedMin;
 		MovementSpeedMax = reference.MovementSpeedMax;
-		RaycastDirection = reference.RaycastDirection;
-		RaycastDistance = reference.RaycastDistance;
-		Mask = reference.Mask;
-		GameObjectOnNextFoot = reference.GameObjectOnNextFoot;
 	}
 
 	public void OnCreate()
@@ -65,22 +60,21 @@ public class Civile : DamageableBase, IPoolable, ICopyable<Civile>
 
 	private void CheckFloorInFront()
 	{
-		Vector2 rayDirection = RaycastDirection;
-		if (transform.forward.z < 0)
+		ForwardGroundGameObject = GroundRayCast.GetHitGameObject(transform.position);
+		if (!ForwardGroundGameObject || FlipWhenSeeing.Contains(ForwardGroundGameObject.layer))
 		{
-			rayDirection = new Vector2(RaycastDirection.x * -1, RaycastDirection.y);
-		}
-		RaycastHit2D hit = Physics2D.Raycast(transform.position, rayDirection, RaycastDistance, Mask, 0);
-		Debug.DrawRay(transform.position, rayDirection, Color.red);
-		GameObjectOnNextFoot = null;
-		if (hit)
-			GameObjectOnNextFoot = hit.collider.gameObject;
-		else
 			transform.Rotate(new Vector3(0, 180, 0));
+			GroundRayCast.FlipX();
+		}
 	}
 
 	public override void Die()
 	{
 		BehaviourPool<Civile>.Recycle(this);
+	}
+
+	void OnDrawGizmos()
+	{
+		GroundRayCast.DrawGizmos();
 	}
 }
