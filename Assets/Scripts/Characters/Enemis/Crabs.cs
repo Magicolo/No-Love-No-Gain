eade6bca7de7fa5f6f6ac9137ce9102ef52bc638
Richard <Rail.rich@gmail.com>
2015-10-23 23:41:07 -0400

    Crab AI

--- Assets/Scripts/Characters/Enemis/Crabs.cs
+++ Assets/Scripts/Characters/Enemis/Crabs.cs
@@ -1,19 +1,30 @@
 ﻿using UnityEngine;
 using System.Collections;
 using Magicolo;
-using System;
 
-public class Crabs : DamageableBase
+public class Crabs : DamageableBaseBase
 {
 	[Disable]
 	public CrabPince[] Pinces;
 
 	public enum CrabStates { Idle, Charging, Attacking };
 
+	public enum CrabBehaviors { AttackPlayer, AttackPopulation, AttackBuilding, AimlessWalks }
+	public CrabBehaviors CrabBehavior;
+
+	[Disable]
+	public GameObject CrabTarget;
+
+	[Disable]
+	public Vector2 targetMovement;
+
 	[Disable]
 	public CrabStates CrabState;
 
 	public Animator Animator;
+	public Rigidbody2D Body;
+
+	public float MouvementSpeed = 2;
 
 	public override void Die()
 	{
@@ -24,15 +35,66 @@ public class Crabs : DamageableBase
 	{
 		Pinces = GetComponentsInChildren<CrabPince>();
 		Animator = GetComponent<Animator>();
-	}
-
-	public override bool CanBeDamagedBy(DamageSources damageSource)
-	{
-		return false;
+		Body = GetComponent<Rigidbody2D>();
 	}
 
 	public override void OnDamaged()
 	{
+		CrabTarget = null;
+	}
 
+	void Update()
+	{
+		switch (CrabBehavior)
+		{
+			case CrabBehaviors.AimlessWalks: AimlessWalk(); break;
+			case CrabBehaviors.AttackBuilding: AttackBuilding(); break;
+			case CrabBehaviors.AttackPlayer: AttackPlayer(); break;
+			case CrabBehaviors.AttackPopulation: AttackPopulation(); break;
+		}
+	}
+
+	private void AttackPopulation()
+	{
+		if (CrabTarget && CrabTarget.gameObject.activeSelf)
+		{
+			CrabTarget = null;
+		}
+		if (CrabTarget == null)
+		{
+			Civile civile = transform.GetClosest<Civile>(Object.FindObjectsOfType<Civile>());
+			if (civile)
+			{
+				CrabTarget = civile.gameObject;
+			}
+		}
+		if (CrabTarget)
+		{
+			targetMovement = MouvementSpeed * (CrabTarget.transform.position - transform.position).normalized;
+			targetMovement.SetValues(0, Axes.Y);
+		}
+	}
+
+	private void AttackPlayer()
+	{
+		if (CrabTarget == null)
+		{
+			CrabTarget = transform.GetClosest<Player>(Object.FindObjectsOfType<Player>()).gameObject;
+		}
+	}
+
+	private void AttackBuilding()
+	{
+
+	}
+
+	private void AimlessWalk()
+	{
+
+	}
+
+	void FixedUpdate()
+	{
+		Body.AccelerateTowards(targetMovement, 100, Kronos.Enemy.DeltaTime);
 	}
 }
